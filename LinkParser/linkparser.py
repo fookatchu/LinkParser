cab8975cc148e36441a218265f293e0b0fe0617d
Heiko <heiko@orgizm.net>
2016-01-27 13:40:01 +0100

    rewrite for stability; prevent download of large files; max size 1mb and only html/text

--- LinkParser/linkparser.py
+++ LinkParser/linkparser.py
@@ -9,11 +9,32 @@ class LinkParser(BotPlugin):
 
     @re_botcmd(prefixed=False, flags=re.IGNORECASE, pattern='(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)')
     def url_match(self, msg, match):
-        r = requests.get('https://noembed.com/embed', params={'url': match.groups()[0]}, verify=False)
-        resp = r.json()
-        if 'error' in resp.keys():
-            r = requests.get(match.groups()[0], verify=False)
-            title = bs4.BeautifulSoup(r.text, 'html.parser').title.string
-        else:
-            title = resp['title']
-        return '[{}]'.format(title.strip())
+        url = match.groups()[0]
+        self.log.info('got url: {}'.format(url))
+        for parser in [parse_noembed, parse_soup]:
+            try:
+                title = parser(url)
+                return '[{}]'.format(title.strip())
+            except:
+                self.log.debug('parser {} failed'.format(parser))
+                continue
+        self.log.info('no info found for url {}!'.format(url))
+
+
+def parse_noembed(url):
+    r = requests.get('https://noembed.com/embed', params={'url': url}, verify=False)
+    resp = r.json()
+    if 'error' in resp.keys():
+        raise UserWarning('noembed found nothing!')
+    return resp['title'].strip()
+
+
+def parse_soup(url):
+    r = requests.get(url, verify=False, stream=True, timeout=2)
+    if 'text/html' not in r.headers['content-type']:
+        raise UserWarning('not an html file!')
+    r.raw.decode_content = True
+    content = r.raw.read(100000 + 1)
+    title = bs4.BeautifulSoup(content, 'html.parser').title.string
+    title = title.replace('\n', ' ').replace('\r', ' ').strip()
+    return title
